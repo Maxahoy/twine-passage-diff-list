eafc5959a999f0129b97d296baf3dc1af6904388
Maxahoy <maxahoy@gmail.com>
2022-12-09 15:42:23 -0500

    changed test structure

--- run_tests.py
+++ run_tests.py
@@ -1,3 +1,14 @@
 #run test suite inside test folder
 import pytest
 import os
+import sys
+from tests import filefinder_tests
+from lib import filefinder
+
+class MyPlugin:
+    def pytest_sessionfinish(self):
+        print("*** test run reporting finished")
+
+if __name__ == "__main__":
+    filefinder_tests.basic_test()
+    sys.exit()
