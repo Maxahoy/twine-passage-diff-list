b1042cb747868c69ad23939afd1b39536204c3a9
Maxahoy <maxahoy@gmail.com>
2022-12-12 19:39:36 -0500

    added function to find common files and extraneous files

--- src/mypkg/filefinder.py
+++ src/mypkg/filefinder.py
@@ -49,5 +49,28 @@ def return_file_lists(original_input_folder, modded_input_folder, filetype="", n
 
     return original_file_list, modded_file_list
 
+def return_common_files(original_file_list, modded_file_list, original_repo_name, modded_repo_name):
+    #returns all files that share a directory + filename with something in the other structure
+    #IE, if both contain a /home/user/original/twine/tests/file.twee and a /home/user/original/modded/twine/tests/file.twee then that directory (truncated to be inside of the respective repos) will be returned
+    # but if one repo contains a twine/tests/file.twee and the other twine/src/file.twee, then that won't be returned
+
+    #go through all the files in the original list truncate until after the appearance of "original repo name"
+    # do the same operation except using the modded_repo_name on the other list
+    original_file_list_reduced = original_file_list.split(original_repo_name, 1)[1]
+    modded_file_list_reduced = modded_file_list.split(modded_repo_name, 1)[1]
+
+    common = original_file_list_reduced.intersection(modded-file_list_reduced)
+    return common
+
+def modded_extra_files(original_file_list, modded_file_list, original_repo_name, modded_repo_name):
+    original_file_list_reduced = original_file_list.split(original_repo_name, 1)[1]
+    modded_file_list_reduced = modded_file_list.split(modded_repo_name, 1)[1]
+
+    common = original_file_list_reduced.intersection(modded-file_list_reduced)
+    #extras = modded_file_list_reduced - common
+    extras = [filename for filename in modded_file_list_reduced if filename not in original_file_list_reduced]
+    return extras
+
+
 
 
